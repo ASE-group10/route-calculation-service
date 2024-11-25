8a2c1b9b03e23b53bb2f094fb1f0e1a753229af2
Zhaocun <zhaocunsun@gmail.com>
2024-11-25 16:32:05 +0000

    updated test file to pass mvn build

--- src/test/java/nl/ase_wayfinding/routecalc/RouteCalculationServiceApplicationTests.java
+++ src/test/java/nl/ase_wayfinding/routecalc/RouteCalculationServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package org.tcd.routecalc;
+package nl.ase_wayfinding.routecalc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = RouteCalculationServiceApplication.class) // Specify the main application class
 class RouteCalculationServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Test that the application context loads successfully
 	}
-
 }
