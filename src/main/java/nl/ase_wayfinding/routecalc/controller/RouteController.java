49cd09513974a4b74da2a9f0c1c19db105d4cb96
ikarthikeya <mandakarthikeya99@gmail.com>
2025-02-27 14:49:56 +0000

    Minor import fixes

--- src/main/java/nl/ase_wayfinding/routecalc/controller/RouteController.java
+++ src/main/java/nl/ase_wayfinding/routecalc/controller/RouteController.java
@@ -1,11 +1,12 @@
 package nl.ase_wayfinding.routecalc.controller;
 
 import com.graphhopper.GHResponse;
-import org.routing.service.GraphHopperService;
+import nl.ase_wayfinding.routecalc.service.GraphHopperService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:3000") // Allow frontend origin
 @RestController
 @RequestMapping("/route")
 public class RouteController {
@@ -19,15 +20,14 @@ public class RouteController {
     public String getRoute(@RequestParam double fromLat, @RequestParam double fromLon,
                            @RequestParam double toLat, @RequestParam double toLon) {
         GHResponse response = graphHopperService.getRoute(fromLat, fromLon, toLat, toLon);
-
         if (response.hasErrors()) {
             return "Error: " + response.getErrors();
         }
-
         return "Distance: " + response.getBest().getDistance() / 1000.0 + " km, "
                 + "Time: " + response.getBest().getTime() / 60000.0 + " minutes.";
     }
-    // New endpoint to get all waypoints between start and end points
+
+    @CrossOrigin(origins = "http://localhost:3000") // Allow frontend origin
     @GetMapping("/waypoints")
     public Map<String, Object> getWaypoints(@RequestParam double fromLat, @RequestParam double fromLon,
                                             @RequestParam double toLat, @RequestParam double toLon) {
