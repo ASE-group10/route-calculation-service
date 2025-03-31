dcf22280f89348794ce83ebf24c0c347b60a2e0a
ZeYanLiow <bryan.liow.zy@gmail.com>
2025-03-31 23:29:47 +0100

    fix: CORS issue

---
+++ src/main/java/nl/ase_wayfinding/routecalc/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package nl.ase_wayfinding.routecalc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(CsrfConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .antMatchers("/route").permitAll()
+                        .antMatchers("/health").permitAll()
+                        .antMatchers("/swagger-ui/**").permitAll()
+                        .antMatchers("/v3/api-docs/**").permitAll()
+                        .antMatchers("/v3/api-docs.yaml").permitAll()
+                        .antMatchers("/swagger-ui.html").permitAll()
+                        .antMatchers("/swagger-resources/**").permitAll()
+                        .antMatchers("/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
